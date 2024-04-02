6ed2e9fc1d54e023f59bd30988af72441a69e23c
Tovi Levis <tovi@fhenix.io>
2024-04-02 20:27:28 +0300

    set trailingSlash to false

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
+  trailingSlash: false,
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
