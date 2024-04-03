e6b9524bdfdab14c39a289186e7e59f33b066a92
Tovi Levis <tovi@fhenix.io>
2024-04-03 10:39:59 +0300

    Fix search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -165,7 +165,7 @@ const config: Config = {
       indexName: 'fhenix',
 
       // Optional: see doc section below
-      contextualSearch: true,
+      contextualSearch: false,
 
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
       //externalUrlRegex: 'external\\.com|domain\\.com',
@@ -177,10 +177,11 @@ const config: Config = {
       // },
 
       // Optional: Algolia search parameters
-      //searchParameters: {},
+      // searchParameters: {},
 
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: 'search',
+
+      // // Optional: path for search page that enabled by default (`false` to disable it)
+      // searchPagePath: 'search',
 
       //... other Algolia params
     },
